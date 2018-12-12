7d2e30ec46fa23284f4f2311b17b0aa419e9f5eb
ajaydas1 <45808704+ajaydas1@users.noreply.github.com>
2018-12-12 09:25:28 +0530

    Update and rename add.py to add and subtract.py

--- add and subtract.py
+++ add and subtract.py
@@ -1,5 +1,9 @@
-def myadd(a,b)
-return a+b
+def myadd(a,b):
+ return a+b
+def mysub(a,b):
+ return (a-b)
 x = int(input("enter first no"))
 y = int(input("enter second no"))
 print(x ,"+", y , "=" ,myadd(x,y))
+print(x ,"+", y , "=" ,mysub(x,y))
+
